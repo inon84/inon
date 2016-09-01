30bb25293e0efe538381af9e53eaaf71634fe2c0
inon84 <inon84@gmail.com>
2016-09-01 16:01:51 +0300

    Changes to be committed: 	modified:   update-apt.sh

--- update-apt.sh
+++ update-apt.sh
@@ -1,1 +1,1 @@
-echo ***Cleaning*** && sudo apt-get clean && sudo apt-get autoclean && echo ***Updating*** && sudo apt-get update && echo ***Upgrading*** && sudo apt-get upgrade
+sudo apt-get clean && sudo apt-get autoclean && echo ***Cleaning Done*** && sudo apt-get update && echo ***Updating Done*** && echo ***Upgrading...*** && sudo apt-get upgrade
