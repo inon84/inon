1d96a6896cc681f2bbf1c6719a16dc7ba6aa93ac
Inon Moshkowitz <inon84@gmail.com>
2016-09-13 00:10:10 +0300

    More colors yay.

--- update-apt.sh
+++ update-apt.sh
@@ -1,5 +1,8 @@
+# Inon Moshkowitz 
+
 RED='\033[0;31m' # Red Color
 GREEN='\033[0;32m' # Green Color
 BLUE='\033[0;34m' # Blue Color
 NC='\033[0m' # No Color
-echo ${BLUE}Starting Auto Clean, Update, Upgrade for apt.${NC} && echo ${RED}Cleaning...${NC} && sudo apt-get clean && sudo apt-get autoclean && echo ${GREEN}Cleaning Done.${NC} && echo ${RED}Updating...${NC} && sudo apt-get update && echo ${GREEN}Updating Done.${NC} && echo ${RED}Checking for upgrades...${NC} && sudo apt-get upgrade && echo ${GREEN}Done. Upgrade/Remove if needed.${NC}
+echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\* && echo ${GREEN}Started Auto Clean+Update+Upgrade bash script for apt-get.${NC} && echo ${RED}Cleaning...${BLUE} && sudo apt-get clean && sudo apt-get autoclean && echo ${GREEN}Cleaning Done.${NC} && echo ${RED}Updating...${BLUE} && sudo apt-get update && echo ${GREEN}Update Done.${NC} && echo ${RED}Checking for upgrades...${BLUE} && sudo apt-get upgrade && echo ${GREEN}All Done. && echo ${GREEN}Upgrade/Remove if needed.${NC} && echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+
