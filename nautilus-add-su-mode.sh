59d886ccbcfed62e4bc6695eae8b2d0675c10bad
Inon Moshkowitz <inon84@gmail.com>
2016-10-06 13:25:24 +0300

    Update nautilus-add-su-mode.sh

--- nautilus-add-su-mode.sh
+++ nautilus-add-su-mode.sh
@@ -1,6 +1,6 @@
 # To install Nautilus Open as Administrator mode in Ubuntu
 # because "gksu nautilus" is not recommended to use.
-
+# read further @ http://www.webupd8.org/2015/03/how-to-run-gedit-and-nautilus-as-root.html
 sudo apt-get install nautilus-admin
 # then restart Nautilus:
 nautilus -q
