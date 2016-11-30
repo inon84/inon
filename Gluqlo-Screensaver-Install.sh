e85d3a3e388fbbf8ac12728e085e7e888f7fdda7
Inon Moshkowitz <inon84@gmail.com>
2016-11-30 15:03:10 +0200

    modified:   Gluqlo-Screensaver-Install.sh

--- Gluqlo-Screensaver-Install.sh
+++ Gluqlo-Screensaver-Install.sh
@@ -1,8 +1,13 @@
 # Gluqlo is a Flip Clock Screensaver for Linux
 # url: http://www.omgubuntu.co.uk/2016/11/gluqlo-flipqlo-screensaver-ubuntu
 
+# var's
+RED='\033[0;31m' # Red Color
 BLUE='\033[0;34m' # Blue Color
+NC='\033[0m' # No Color
 
+echo Installing Gluqlo Flip Clock Screensaver for Linux, removing gnome-screensaver...
+echo 
 sudo apt-get install xscreensaver xscreensaver-gl-extra xscreensaver-data-extra
 sudo apt-get remove gnome-screensaver
 sudo apt-add-repository ppa:alexanderk23/ppa
