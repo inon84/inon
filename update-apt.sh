e3d168c112841eacb461b1f910a5a9c8a2b15563
Inon Moshkowitz <inon84@gmail.com>
2016-12-21 00:52:13 +0200

    Changelog: ---------- - Got rid of colors - Switched to apt instead of older apt-get version

--- update-apt.sh
+++ update-apt.sh
@@ -1,8 +1,8 @@
 # Inon Moshkowitz 
 
-RED='\033[0;31m' # Red Color
-GREEN='\033[0;32m' # Green Color
-BLUE='\033[0;34m' # Blue Color
-NC='\033[0m' # No Color
-echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\* && echo ${GREEN}Started Auto Clean+Update+Upgrade bash script for apt-get.${NC} && echo ${RED}Cleaning...${BLUE} && sudo apt-get clean && sudo apt-get autoclean && echo ${GREEN}Cleaning Done.${NC} && echo ${RED}Updating...${BLUE} && sudo apt-get update && echo ${GREEN}Update Done.${NC} && echo ${RED}Checking for upgrades...${BLUE} && sudo apt-get upgrade && echo ${GREEN}All Done. && echo ${GREEN}Upgrade/Remove if needed.${NC} && echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+#RED='\033[0;31m' # Red Color
+#GREEN='\033[0;32m' # Green Color
+#BLUE='\033[0;34m' # Blue Color
+#NC='\033[0m' # No Color
+echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\* && echo Started Auto Clean+Update+Upgrade bash script for apt-get. && echo Cleaning... && sudo apt-get clean && sudo apt-get autoclean && echo Cleaning Done. && echo Updating... && sudo apt update && echo Update done. && echo Checking for upgrades... && sudo apt upgrade && echo All done. && date -R && echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
 
