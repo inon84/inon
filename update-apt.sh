7cb4dbdf97fb6837123ceadf5b24bcca9fe39f6d
Inon Moshkowitz <inon84@gmail.com>
2016-12-22 11:33:08 +0200

    got rid of the one liner

--- update-apt.sh
+++ update-apt.sh
@@ -4,5 +4,20 @@
 #GREEN='\033[0;32m' # Green Color
 #BLUE='\033[0;34m' # Blue Color
 #NC='\033[0m' # No Color
-echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\* && echo Started Auto Clean+Update+Upgrade bash script for apt-get. && echo Cleaning... && sudo apt-get clean && sudo apt-get autoclean && echo Cleaning Done. && echo Updating... && sudo apt update && echo Update done. && echo Checking for upgrades... && sudo apt upgrade && echo All done. && date -R && echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
+echo ">> Started bash script for apt updates"
+echo ">> Cleaning"
+sudo apt clean
+sudo apt autoclean
+echo " "
+echo ">> Cleaning Done"
+echo ">> Updating"
+sudo apt update
+echo ">> Updating done"
+echo " "
+echo ">> Checking for upgrades"
+sudo apt upgrade
+echo " "
+echo "Script last executed at: " && date -R
+echo \*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*\*
 
